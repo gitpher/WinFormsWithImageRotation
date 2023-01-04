7761cc2ea775acf63ff54aaddb87b825abd2c23e
gitpher <jun_chois@naver.com>
2023-01-04 20:35:31 +0900

    helpless

--- WinFormsWithImageRotation/Form1.cs
+++ WinFormsWithImageRotation/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -22,26 +24,24 @@ namespace WinFormsWithImageRotation
             InitializeComponent();
         }
 
-        // Are there any better way to import dll? Where should I place these arguments?
         [DllImport("RotateImg.dll")]
-        public extern static Mat readImage([MarshalAs(UnmanagedType.LPStr)]string imgPath); // 
+        public static extern byte[] readImg([MarshalAs(UnmanagedType.LPStr)]string imgPath);
 
         [DllImport("RotateImg.dll")]
-        public extern static double getRadian(double angle);
+        public static extern double getRadian(double angle);
 
-        [DllImport("RotateImg.dll")]
-        public extern static Mat createDstImg(Mat srcImg, double angle); // 
+        //////////////////////////////////////
+        private string imgPath;
+        
+        private Image srcImg;
+        private Image dstImg;
 
-        [DllImport("RotateImg.dll")]
-        public extern static Mat fillDstImg(Mat dstImg, Mat srcImg, double radian); // 
+        private IntPtr srcImgPtr;
+        private IntPtr dstImgPtr;
 
-        // Not sure this is the right way to do it
-        // byte array
-        string imgPath;
-        Image srcImg; // 
-        Image dstImg; //
-        double angle;
-        double radian;
+        private double angle;
+        private double radian;
+        //////////////////////////////////////
 
         private void Browse_Click(object sender, EventArgs e)
         {
@@ -57,22 +57,18 @@ namespace WinFormsWithImageRotation
                 
                 ImagePathBox.Text = imgPath;
 
+                byte[] bytes = readImg(imgPath);
 
-                Image srcImg = Image.FromFile(imgPath);
-                
-                // transition from Image to byte array
-                var ms = new MemoryStream();
-                srcImg.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
-                var bytes = ms.ToArray();
-                
-                // transition from byte array to Image
-                var imgMemoryStream = new MemoryStream(bytes);
+                Bitmap bmp;
+                using (var ms = new MemoryStream(bytes))
+                {
+                    bmp = new Bitmap(ms);
+                }
 
-                Image imgFromStream = Image.FromStream(imgMemoryStream);
+                OriginalImage.Image = bmp;
 
-                // display Image on OriginalImagePictureBox
-                OriginalImage.Image = srcImg;
-                //OriginalImage.Image = imgFromStream;
+
+
             }
         }
 
@@ -88,5 +84,7 @@ namespace WinFormsWithImageRotation
 
             //RotatedImage.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(filledDstImg);
         }
+
+        
     }
 }
