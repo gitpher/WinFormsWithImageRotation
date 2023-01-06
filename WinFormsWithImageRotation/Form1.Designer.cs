34c4cd3783f71170a60dfce0575126ff9f18cdea
gitpher <jun_chois@naver.com>
2023-01-06 17:28:04 +0900

    WinFormsWithImageRotation ver.2.0.0

--- WinFormsWithImageRotation/Form1.Designer.cs
+++ WinFormsWithImageRotation/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.RotatedImageLabel = new System.Windows.Forms.Label();
             this.RotateDegreeNumericUpDown = new System.Windows.Forms.NumericUpDown();
             this.ImagePathBox = new System.Windows.Forms.TextBox();
+            this.Download = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.OriginalImage)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.RotatedImage)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.RotateDegreeNumericUpDown)).BeginInit();
@@ -43,38 +44,40 @@
             // 
             // Browse
             // 
-            this.Browse.Location = new System.Drawing.Point(543, 77);
+            this.Browse.Location = new System.Drawing.Point(618, 27);
             this.Browse.Name = "Browse";
             this.Browse.Size = new System.Drawing.Size(75, 23);
             this.Browse.TabIndex = 0;
-            this.Browse.Text = "Browse";
+            this.Browse.Text = "파일 열기";
             this.Browse.UseVisualStyleBackColor = true;
             this.Browse.Click += new System.EventHandler(this.Browse_Click);
             // 
             // Rotate
             // 
-            this.Rotate.Location = new System.Drawing.Point(543, 117);
+            this.Rotate.Location = new System.Drawing.Point(618, 55);
             this.Rotate.Name = "Rotate";
             this.Rotate.Size = new System.Drawing.Size(75, 23);
             this.Rotate.TabIndex = 1;
-            this.Rotate.Text = "Rotate";
+            this.Rotate.Text = "회전 각도";
             this.Rotate.UseVisualStyleBackColor = true;
             this.Rotate.Click += new System.EventHandler(this.Rotate_Click);
             // 
             // OriginalImage
             // 
-            this.OriginalImage.Location = new System.Drawing.Point(100, 210);
+            this.OriginalImage.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.OriginalImage.Location = new System.Drawing.Point(58, 143);
             this.OriginalImage.Name = "OriginalImage";
-            this.OriginalImage.Size = new System.Drawing.Size(232, 218);
+            this.OriginalImage.Size = new System.Drawing.Size(300, 300);
             this.OriginalImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.OriginalImage.TabIndex = 2;
             this.OriginalImage.TabStop = false;
             // 
             // RotatedImage
             // 
-            this.RotatedImage.Location = new System.Drawing.Point(415, 210);
+            this.RotatedImage.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.RotatedImage.Location = new System.Drawing.Point(393, 143);
             this.RotatedImage.Name = "RotatedImage";
-            this.RotatedImage.Size = new System.Drawing.Size(218, 218);
+            this.RotatedImage.Size = new System.Drawing.Size(300, 300);
             this.RotatedImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.RotatedImage.TabIndex = 3;
             this.RotatedImage.TabStop = false;
@@ -82,32 +85,34 @@
             // OriginalImageLabel
             // 
             this.OriginalImageLabel.AutoSize = true;
-            this.OriginalImageLabel.Location = new System.Drawing.Point(179, 188);
+            this.OriginalImageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OriginalImageLabel.Location = new System.Drawing.Point(172, 117);
             this.OriginalImageLabel.Name = "OriginalImageLabel";
-            this.OriginalImageLabel.Size = new System.Drawing.Size(74, 13);
+            this.OriginalImageLabel.Size = new System.Drawing.Size(73, 20);
             this.OriginalImageLabel.TabIndex = 4;
-            this.OriginalImageLabel.Text = "Original Image";
+            this.OriginalImageLabel.Text = "원본 이미지";
             // 
             // RotatedImageLabel
             // 
             this.RotatedImageLabel.AutoSize = true;
-            this.RotatedImageLabel.Location = new System.Drawing.Point(483, 188);
+            this.RotatedImageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RotatedImageLabel.Location = new System.Drawing.Point(503, 117);
             this.RotatedImageLabel.Name = "RotatedImageLabel";
-            this.RotatedImageLabel.Size = new System.Drawing.Size(77, 13);
+            this.RotatedImageLabel.Size = new System.Drawing.Size(85, 20);
             this.RotatedImageLabel.TabIndex = 5;
-            this.RotatedImageLabel.Text = "Rotated Image";
+            this.RotatedImageLabel.Text = "회전된 이미지";
             // 
             // RotateDegreeNumericUpDown
             // 
             this.RotateDegreeNumericUpDown.DecimalPlaces = 1;
-            this.RotateDegreeNumericUpDown.Location = new System.Drawing.Point(417, 117);
+            this.RotateDegreeNumericUpDown.Location = new System.Drawing.Point(492, 56);
             this.RotateDegreeNumericUpDown.Maximum = new decimal(new int[] {
-            360,
+            1000,
             0,
             0,
             0});
             this.RotateDegreeNumericUpDown.Minimum = new decimal(new int[] {
-            360,
+            1000,
             0,
             0,
             -2147483648});
@@ -117,16 +122,28 @@
             // 
             // ImagePathBox
             // 
-            this.ImagePathBox.Location = new System.Drawing.Point(122, 77);
+            this.ImagePathBox.Location = new System.Drawing.Point(58, 29);
             this.ImagePathBox.Name = "ImagePathBox";
-            this.ImagePathBox.Size = new System.Drawing.Size(415, 20);
+            this.ImagePathBox.Size = new System.Drawing.Size(554, 20);
             this.ImagePathBox.TabIndex = 8;
+            // 
+            // Download
+            // 
+            this.Download.Location = new System.Drawing.Point(618, 458);
+            this.Download.Name = "Download";
+            this.Download.Size = new System.Drawing.Size(75, 23);
+            this.Download.TabIndex = 9;
+            this.Download.Text = "다운로드";
+            this.Download.UseVisualStyleBackColor = true;
+            this.Download.Click += new System.EventHandler(this.Download_Click);
             // 
             // ImageRotationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(750, 470);
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(749, 498);
+            this.Controls.Add(this.Download);
             this.Controls.Add(this.ImagePathBox);
             this.Controls.Add(this.RotateDegreeNumericUpDown);
             this.Controls.Add(this.RotatedImageLabel);
@@ -135,6 +152,7 @@
             this.Controls.Add(this.OriginalImage);
             this.Controls.Add(this.Rotate);
             this.Controls.Add(this.Browse);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "ImageRotationForm";
             this.Text = "ImageRotationForm";
@@ -156,6 +174,7 @@
         private System.Windows.Forms.Label RotatedImageLabel;
         private System.Windows.Forms.NumericUpDown RotateDegreeNumericUpDown;
         private System.Windows.Forms.TextBox ImagePathBox;
+        private System.Windows.Forms.Button Download;
     }
 }
 
--- WinFormsWithImageRotation/Form1.cs
+++ WinFormsWithImageRotation/Form1.cs
@@ -12,6 +12,7 @@ using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 using System.Xml.Linq;
 using OpenCvSharp;
 using OpenCvSharp.Extensions;
@@ -38,18 +39,16 @@ namespace WinFormsWithImageRotation
         public static extern IntPtr readImg([MarshalAs(UnmanagedType.LPStr)] string imgPath);
 
         [DllImport("RotateImg.dll")]
-        public static extern double getRadian(double angle);
+        public static extern IntPtr RotateImg(IntPtr srcImg, double angle);
 
         [DllImport("RotateImg.dll")]
-        public static extern IntPtr createDstImg(IntPtr srcImg, double angle);
+        public static extern void deleteImg(IntPtr img);
 
-        [DllImport("RotateImg.dll")]
-        public static extern void fillDstImg(IntPtr dstImg, IntPtr srcImg, double radian);
-
-        [DllImport("RotateImg.dll")]
-        public static extern IntPtr happyDay();
-
+        int pictureBoxDefaultWidth = 300;
+        int pictureBoxDefaultHeight = 300;
+        string imgPath;
         IntPtr srcImgPtr;
+        IntPtr dstImgPtr;
 
         private void Browse_Click(object sender, EventArgs e)
         {
@@ -61,9 +60,14 @@ namespace WinFormsWithImageRotation
 
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                string imgPath = openFileDialog.FileName;
-                
+                if (srcImgPtr != IntPtr.Zero) deleteImg(srcImgPtr);
+
+                imgPath = openFileDialog.FileName;
+
                 ImagePathBox.Text = imgPath;
+
+                OriginalImage.Size = new System.Drawing.Size(pictureBoxDefaultWidth, pictureBoxDefaultHeight);
+                RotatedImage.Size = new System.Drawing.Size(pictureBoxDefaultWidth, pictureBoxDefaultHeight);
 
                 srcImgPtr = readImg(imgPath);
 
@@ -76,28 +80,69 @@ namespace WinFormsWithImageRotation
                     OriginalImage.Image = srcImgbmp;
 
                     RotatedImage.Image = srcImgbmp;
+
+                    double aspectRatio = srcImg->width / (double)srcImg->height;
+
+                    if (aspectRatio > 1)
+                    {
+                        OriginalImage.Height = (int)(OriginalImage.Width / aspectRatio);
+                        RotatedImage.Height = (int)(RotatedImage.Width / aspectRatio);
+                    }
+                    else
+                    {
+                        OriginalImage.Width = (int)(OriginalImage.Height * aspectRatio);
+                        RotatedImage.Width = (int)(RotatedImage.Height * aspectRatio);
+                    }
+
+                    RotateDegreeNumericUpDown.Value = 0;
                 }
             }
         }
 
         private void Rotate_Click(object sender, EventArgs e)
         {
+            if (dstImgPtr != IntPtr.Zero) deleteImg(dstImgPtr);
+
             double angle = (double) RotateDegreeNumericUpDown.Value;
 
-            double radian = getRadian(angle);
-
-            IntPtr dstImgPtr = createDstImg(srcImgPtr, angle);
+            dstImgPtr = RotateImg(srcImgPtr, angle);
 
             unsafe
             {
                 IMAGE* dstImg = (IMAGE*) dstImgPtr.ToPointer();
 
-                fillDstImg(dstImgPtr, srcImgPtr, radian);
-
                 Bitmap dstImgBmp = new Bitmap(dstImg->width, dstImg->height, dstImg->width * dstImg->channels, PixelFormat.Format32bppArgb, dstImg->data);
-                
+
+                double aspectRatio = dstImg->width / (double)dstImg->height;
+
+                if (aspectRatio > 1)
+                {
+                    RotatedImage.Width = pictureBoxDefaultWidth;
+                    RotatedImage.Height = (int)(RotatedImage.Width / aspectRatio);
+                }
+                else
+                {
+                    RotatedImage.Height = pictureBoxDefaultHeight;
+                    RotatedImage.Width = (int)(RotatedImage.Height * aspectRatio);
+                }
+
                 RotatedImage.Image = dstImgBmp;
             }
+        }
+
+        private void Download_Click(object sender, EventArgs e)
+        {
+            double rotatedDegree = (double)RotateDegreeNumericUpDown.Value;
+            string filename = Path.GetFileNameWithoutExtension(imgPath) + "(" + rotatedDegree + "도 회전).png";
+            string location = @"C:\Users\jun_c\Downloads\";
+
+            RotatedImage.Image.Save(location + filename, ImageFormat.Png);
+
+            string title = "이미지 저장 성공";
+            string message = "이미지가 저장되었습니다." + "\n" +
+                            "(다운로드 위치: " + location + filename + ")";
+
+            MessageBox.Show(message, title);
         }
     }
 }
